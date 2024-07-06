48039454ad53720819c0a4c7e0a5daa87877651b
devEliasJr <eliasengmecanica@gmail.com>
2024-07-06 14:16:28 -0300

    feat: Tweaks at sigin form and validations with formik and yup

--- src/Pages/Sign-In.tsx
+++ src/Pages/Sign-In.tsx
@@ -1,62 +1,67 @@
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
+import { Field, FieldProps, Form, Formik } from "formik";
+import * as Yup from "yup";
+import { FormControl } from "@mui/material";
+import { dotPulse } from "ldrs";
+import theme from "../theme";
+import { useNavigate } from "react-router-dom";
 
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
+interface LoginFormValues {
+  email: string;
+  password: string;
 }
 
 export default function SignInSide() {
+  const navigate = useNavigate();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Email inválido").required("Email é obrigatório"),
+    password: Yup.string()
+      .min(6, "A senha deve ter pelo menos 6 caracteres")
+      .required("Senha é obrigatória"),
+  });
+
+  const onSubmit = (
+    values: LoginFormValues,
+    { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
+  ) => {
+    try {
+      setTimeout(() => {
+        console.log(`Logando: ${(values.email, values.password)}`);
+        setSubmitting(false);
+        navigate("/dashboard");
+      }, 1000);
+    } catch (error) {
+      setSubmitting(false);
+    }
   };
 
+  dotPulse.register();
 
   return (
     <Grid container component="main" sx={{ height: "100vh" }}>
       <Grid
-  item
-  xs={false}
-  sm={4}
-  md={7}
-  sx={{
-    backgroundImage: `url("https://picsum.photos/1920/1080?grayscale")`,
-    backgroundColor: (t) =>
-      t.palette.mode === "light"
-        ? t.palette.grey[900]
-        : t.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "left",
-  }}
-/>
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: `url("https://picsum.photos/1920/1080?grayscale")`,
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[900]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "left",
+        }}
+      />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <Box
           sx={{
@@ -73,58 +78,64 @@ export default function SignInSide() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ mt: 5 }} />
-          </Box>
+            {({ isSubmitting, errors, touched }) => (
+              <Form>
+                <Box sx={{ mt: 2 }} minWidth={"100%"}>
+                  <FormControl sx={{ pb: 2 }} fullWidth>
+                    <Field name="email">
+                      {({ field }: { field: FieldProps }) => (
+                        <TextField
+                          {...field}
+                          label="Email"
+                          variant="outlined"
+                          error={touched.email && Boolean(errors.email)}
+                          helperText={touched.email && errors.email}
+                        />
+                      )}
+                    </Field>
+                  </FormControl>
+                  <FormControl sx={{ pb: 2 }} fullWidth>
+                    <Field name="password">
+                      {({ field }: { field: FieldProps }) => (
+                        <TextField
+                          {...field}
+                          label="Senha"
+                          type="password"
+                          variant="outlined"
+                          error={touched.password && Boolean(errors.password)}
+                          helperText={touched.password && errors.password}
+                        />
+                      )}
+                    </Field>
+                  </FormControl>
+
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    disabled={isSubmitting}
+                    sx={{ mt: 3, mb: 2, height: 40, minWidth: 200 }}
+                  >
+                    {isSubmitting ? (
+                      <l-dot-pulse
+                        size="40"
+                        speed="1.3"
+                        color={theme.palette.grey[400]}
+                      ></l-dot-pulse>
+                    ) : (
+                      "Entrar"
+                    )}
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
         </Box>
       </Grid>
     </Grid>
